9fcf6f2aca3ce14c0e463f380c039c86ecc92547
Marius Degenhardt <marius.degenhardt@stud.uni-goettingen.de>
2025-05-11 18:25:51 +0200

    finsihed 1b

---
+++ Exercise02/exercise2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# 1a
+mkdir GG
+cd GG
+curl -o gg.txt https://raw.githubusercontent.com/levinalex/deutsche_verfassungen/master/grundgesetz/grundgesetz.txt 
+
+# 1b
+# from line 0 delete everything until "I\nDie Grundrechte"
+# credits: a little bit of chatgpt and alot of thinking
+# //!d = //=match previous regex !=not d=delete
+# /b = branching. If no branch is defined, it will skip to the end
+sed -e '/^I$/b' -e '0, /Die Grundrechte/{//!d}' gg.txt > gg_bereinigt.txt
+
+
+

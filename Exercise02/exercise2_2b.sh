3a7a6cc7e8118f825a4f29ee5bb43ca4444c8ff4
Marius Degenhardt <marius.degenhardt@stud.uni-goettingen.de>
2025-05-20 17:41:06 +0200

    worked on solution

--- Exercise02/exercise2_2b.sh
+++ Exercise02/exercise2_2b.sh
@@ -1,49 +1,26 @@
 #!/bin/bash
 
-#2b
-create_indent ()
-{   
-    # $# argument count
-    # [] = test function
-    if [ "$#" -ne 1 ]; then
-        echo "[indent] Error, insufficient parameters"
-        exit
-    fi
-
-    indent_depth="$1"
-
-    INDENTS=""
-
-    # seq = sequence like range in python
-    #$() = run command
-    for _ in $(seq "$indent_depth"); do
-        INDENTS+="    "
-    done
-}
-
 walk_dirs ()
 {
     # $1 $2 arguments thats getting passed to walk_dirs
     path="$1"
 
     if [ "$#" -eq 1 ]; then
-        indent=0
+        indent=''
     else
         indent="$2"
     fi
 
     # loop over current directory
-    for entry in "$path"/*; do
-        create_indent "$indent"
-        
+    for entry in "$path"/*; do        
         # test if the entry is an directory
         if [ -d "$entry" ]; then
-            # print the dir with indentation and remove leading / from entry
-            printf "%sDirectory: %s\n" "$INDENTS" "${entry:1}"
+            # print the dir with indentation 
+            printf "%sDirectory: %s\n" "$indent" "$entry"
             # call walk_dir for the children dir and increment the indent
-            walk_dirs "$entry" $(("$indent + 1"))
+            walk_dirs "$entry" "    $indent"
         else
-            printf "%sFile: %s\n" "$INDENTS" "${entry:1}"
+            printf "%sFile: %s\n" "$indent" "$entry"
         fi
     done
 }
